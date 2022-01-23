cf294aa7d8e2972d2d6b393eb4f7cef3ddbac4a3
Cloudnextgeneration <info@cloudnextbd.com>
2022-01-23 12:15:11 +0600

    first hello print & run, values and variable testing in golang invironment

---
+++ variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	var b, c int = 1, 2
+	var d = true
+	var e int
+	f := "apple"
+	var g = 100
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	a = "faisal"
+	fmt.Println(a)
+}
